204db494b7a4216d3af82517a9d54a05c8cddfe5
Joseph Fonseka <hevayo@gmail.com>
2023-07-25 13:59:54 +0530

    Add API Call

--- resident-webapp/src/components/schedules/VisitDetailCard.tsx
+++ resident-webapp/src/components/schedules/VisitDetailCard.tsx
@@ -3,6 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
 
 type Visit = {
     inTime: string;
@@ -29,29 +30,32 @@ const VisitDetailCard: React.FC<VisitDetailCardProps> = ({ visit }) => {
     };
 
     return (
-        <Card sx={{ maxWidth: 400 }}>
-            <CardContent>
-                <Typography variant="h5" component="div">
-                    Visit Details
-                </Typography>
-                <Typography>
-                    In Time: {visit.inTime} <br />
-                    Out Time: {visit.outTime} <br />
-                    House No: {visit.houseNo} <br />
-                    Visitor Name: {visit.visitorName} <br />
-                    Visitor NIC: {visit.visitorNIC} <br />
-                    Visitor Phone No: {visit.visitorPhoneNo} <br />
-                    Vehicle Number: {visit.vehicleNumber} <br />
-                    Visit Date: {visit.visitDate} <br />
-                    Is Approved: {visit.isApproved ? "Yes" : "No"} <br />
-                    Comment: {visit.comment} <br />
-                    Visit Id: {visit.visitId}
-                </Typography>
-                <Button variant="contained" color="primary" onClick={approveVisit}>
-                    Approve
-                </Button>
-            </CardContent>
-        </Card>
+        <Box>
+            <Typography variant="h5" component="div">
+                Visit Details
+            </Typography>
+            <Card sx={{ maxWidth: 400 }}>
+                <CardContent>
+
+                    <Typography>
+                        In Time: {visit.inTime} <br />
+                        Out Time: {visit.outTime} <br />
+                        House No: {visit.houseNo} <br />
+                        Visitor Name: {visit.visitorName} <br />
+                        Visitor NIC: {visit.visitorNIC} <br />
+                        Visitor Phone No: {visit.visitorPhoneNo} <br />
+                        Vehicle Number: {visit.vehicleNumber} <br />
+                        Visit Date: {visit.visitDate} <br />
+                        Is Approved: {visit.isApproved ? "Yes" : "No"} <br />
+                        Comment: {visit.comment} <br />
+                        Visit Id: {visit.visitId}
+                    </Typography>
+                    <Button variant="contained" color="primary" onClick={approveVisit}>
+                        Approve
+                    </Button>
+                </CardContent>
+            </Card>
+        </Box>
     );
 };
 

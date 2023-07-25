ffde91e8537775c59f98223158cabf5a5a75a179
wx-yz <57770159+wx-yz@users.noreply.github.com>
2023-07-25 14:23:00 +0530

    Adding search to results

--- security-webapp/src/components/Schedules.js
+++ security-webapp/src/components/Schedules.js
@@ -1,14 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { Container, Input} from 'semantic-ui-react';
 import { useAuthContext } from "@asgardeo/auth-react";
-import UnderConstructionMessage from './UnderConstructionMessage';
+// import UnderConstructionMessage from './UnderConstructionMessage';
+import { Table, Header, HeaderCell, HeaderRow, Body, Row, Cell } from '@table-library/react-table-library/table';
 
 function Schedules() {
 
-  const [items, setitems] = useState([]);
-  const [searchTerm, setSearchTerm] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  // const [items, setitems] = useState([]);
+  // const [searchTerm, setSearchTerm] = useState('');
+  // const [searchResults, setSearchResults] = useState([]);
   const baseUrl = process.env.REACT_APP_RESOURCE_SERVER_URL;
+  const [search, setSearch] = useState('');
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
   const { state, httpRequest } = useAuthContext();
   var path = "/items";
@@ -35,30 +40,67 @@ function Schedules() {
     // }
 
     // fetchitems();
+
+
   }, []);
 
-  useEffect(() => {
-    const results = items.filter(item =>
-      item.title.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setSearchResults(results);
-  }, [items, searchTerm]);
+  let mockResults = [
+    { "houseNo": "1", "visitorName": "Mark", "visitorNIC": "1234", "visitorPhoneNo": "123-456-7890", "vehicleNumber": "wp-1234", "visitDate": "7/28/2023", "isApproved": true, "comment": "desc", "visitId": 1 },
+    { "houseNo": "2", "visitorName": "John", "visitorNIC": "5678", "visitorPhoneNo": "123-456-7890", "vehicleNumber": "wp-1234", "visitDate": "7/29/2023", "isApproved": true, "comment": "desc", "visitId": 2 },
+    { "houseNo": "3", "visitorName": "Mary", "visitorNIC": "1234", "visitorPhoneNo": "123-444-7890", "vehicleNumber": "wp-1234", "visitDate": "7/29/2023", "isApproved": true, "comment": "desc", "visitId": 3 },
+    { "houseNo": "4", "visitorName": "Ann", "visitorNIC": "1234", "visitorPhoneNo": "123-555-7890", "vehicleNumber": "wp-1234", "visitDate": "7/30/2023", "isApproved": true, "comment": "desc", "visitId": 4 },
+    { "houseNo": "5", "visitorName": "Bob", "visitorNIC": "1234", "visitorPhoneNo": "777-456-7890", "vehicleNumber": "wp-1234", "visitDate": "7/28/2023", "isApproved": true, "comment": "desc", "visitId": 5 }
+  ];
 
-  const handleSearchChange = event => {
-    setSearchTerm(event.target.value);
+  const data = {
+    nodes: mockResults.filter((item) =>
+      item.visitorName.toLowerCase().includes(search.toLowerCase()) ||
+      item.visitorNIC.includes(search) ||
+      item.visitorPhoneNo.includes(search) ||
+      item.vehicleNumber.includes(search)
+    ),
   };
 
   return (
     <Container>
-      <Input
-        type="text"
-        placeholder="Search by schedule Id"
-        value={searchTerm}
-        onChange={handleSearchChange}
-        fluid
-      />
-      {/* TODO change logic based on the logged in status */}
-     <UnderConstructionMessage/>
+      <label htmlFor="search">
+        Search by Task:
+        <input id="search" type="text" onChange={handleSearch} />
+      </label>
+      <Table data={data}>
+        {(tableList) => (
+        <>
+          <Header>
+            <HeaderRow>
+              <HeaderCell>House #</HeaderCell>
+              <HeaderCell>Visitor Name</HeaderCell>
+              <HeaderCell>NIC</HeaderCell>
+              <HeaderCell>Phone #</HeaderCell>
+              <HeaderCell>Vehicle #</HeaderCell>
+              <HeaderCell>Visit Date</HeaderCell>
+              <HeaderCell>Approved</HeaderCell>
+              <HeaderCell>Comment</HeaderCell>
+              <HeaderCell>Visit Id</HeaderCell>
+            </HeaderRow>
+          </Header>
+          <Body>
+            {tableList.map((item) => (
+              <Row key={item.houseNo} item={item}>
+                <Cell>{item.houseNo}</Cell>
+                <Cell>{item.visitorName}</Cell>
+                <Cell>{item.visitorNIC}</Cell>
+                <Cell>{item.visitorPhoneNo}</Cell>
+                <Cell>{item.vehicleNumber}</Cell>
+                <Cell>{item.visitDate}</Cell>
+                <Cell>{item.isApproved}</Cell>
+                <Cell>{item.comment}</Cell>
+                <Cell>{item.visitId}</Cell>
+              </Row>
+            ))}
+          </Body>
+        </>
+        )}
+    </Table>
     </Container>
   );
 }
